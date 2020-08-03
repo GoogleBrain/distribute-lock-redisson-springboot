1986aa52b12fe98196751b95d654e6f1d13890e6
googlebrain@163.com <googlebrain@163.com>
2020-08-03 22:29:39 +0800

    springboot + redission 实现分布式锁。

---
+++ src/test/java/cn/k/distributelockredissionspringboot/DistributeLockRedissionSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.k.distributelockredissionspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributeLockRedissionSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
